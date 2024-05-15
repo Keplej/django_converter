677116bf43811c317783fa1aeb8878d1306b538b
Keplej <Johnny.Keple@gmail.com>
2024-05-15 12:24:25 -0500

    Finishing admin and project image upload

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+# projects/admin.py
+from projects.models import Project
 
-# Register your models here.
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Project, ProjectAdmin)
